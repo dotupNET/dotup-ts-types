f885f3acfcf876ade9adebf5ca7add8101bc8cfa
Peter Ullrich <dotup.software@gmail.com>
2019-04-09 07:40:51 +0200

    -PromiseTools -RegexTools

---
+++ src/PromiseTools.ts
@@ -0,0 +1,41 @@
+// tslint:disable-next-line: no-require-imports
+import { EventEmitter } from 'events';
+
+export async function sleep(ms: number): Promise<void> {
+  // tslint:disable-next-line: no-string-based-set-timeout
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export async function Event<T>(emitter: EventEmitter, event: string, timeout?: number): Promise<T> {
+  return new Promise<T>((resolve, reject) => {
+    emitter.on(event, _ => resolve());
+  });
+}
+
+// tslint:disable-next-line: max-line-length : no-any
+export async function timeoutAfter<T>(timeout: number, executor: (resolve: (value?: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void): Promise<T> {
+  const p = new Promise<T>((resolve, reject) =>
+    setTimeout(
+      () =>
+        reject(`Timed out after ${timeout} ms.`),
+      timeout)
+  );
+
+  return Promise.race<T>([
+    new Promise<T>(executor),
+    p
+  ]);
+}
+
+export async function timeoutPromise<T>(timeout: number, promise: Promise<T>): Promise<T> {
+  const p = new Promise<T>((resolve, reject) =>
+    setTimeout(
+      () => reject(`Timed out after ${timeout} ms.`),
+      timeout)
+  );
+
+  return Promise.race<T>([
+    promise,
+    p
+  ]);
+}
---
+++ src/RegexTools.ts
@@ -0,0 +1,50 @@
+import { IUrl } from './Interfaces';
+
+export namespace RegexTools {
+  export function parseUrl(url: string): IUrl {
+    // tslint:disable-next-line: max-line-length
+    const parts = /(?:(?:(([^:\/#\?]+:)?(?:(?:\/\/)(?:(?:(?:([^:@\/#\?]+)(?:\:([^:@\/#\?]*))?)@)?(([^:\/#\?\]\[]+|\[[^\/\]@#?]+\])(?:\:([0-9]+))?))?)?)?((?:\/?(?:[^\/\?#]+\/+)*)(?:[^\?#]*)))?(\?[^#]+)?)(#.*)?/.exec(url);
+
+    return {
+      href: parts[0],
+      origin: parts[1],
+      protocol: parts[2],
+      username: parts[3],
+      password: parts[4],
+      host: parts[5],
+      hostname: parts[6],
+      port: Number(parts[7]),
+      pathname: parts[8],
+      search: parts[9],
+      hash: parts[10]
+    };
+  }
+
+  /**
+   * Replace placeholder in text with args
+   *
+   * Indexed:
+   * const str = RegexTools.replace('{1} {0}', 'tool', 'Nice'};
+   *
+   * With Object:
+   * const str = RegexTools.replace('{a} {b}', {b: 'tool', a: 'Nice'}};
+   *
+   */
+  export function replace(text: string, ...args: any[]): string {
+    let result = text;
+
+    if (args.length) {
+      const t = typeof args[0];
+      var key;
+      var values: any = ("string" === t || "number" === t) ?
+        Array.prototype.slice.call(args)
+        : args[0];
+
+      for (key in values) {
+        result = result.replace(new RegExp("\\{" + key + "\\}", "gi"), values[key]);
+      }
+    }
+
+    return result;
+  };
+}
