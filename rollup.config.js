13f8c0708f87a894e7d676a4f0a9fbf4bc5c7cfe
Peter Ullrich <dotup.software@gmail.com>
2020-03-06 04:23:55 +0100

    rollup config path

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
 import pkg from "./package.json";
-import { getConfig } from "../../rollup.config";
-import {  } from "../rollup.config";
+import { getConfig } from "../rollup.config";
+
 export default getConfig(pkg, "dotup", "tsconfig.esnext.json");
